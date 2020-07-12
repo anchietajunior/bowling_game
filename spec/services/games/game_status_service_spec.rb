135b47a79dad97255357d174bee7609e2727b5bc
Anchieta Júnior <santosjr87@gmail.com>
2020-07-12 15:48:43 -0300

    Add specs

--- spec/services/games/game_status_service_spec.rb
+++ spec/services/games/game_status_service_spec.rb
@@ -2,15 +2,54 @@ require 'rails_helper'
 
 RSpec.describe Games::GameStatusService do
 
-  let!(:game) { create(:game) }
-
-  let(:service) do
-    described_class.call(game)
-  end
-
   describe '#call' do
-    it 'has a status in_progress' do
-      expect(service.value).to eq({ id: game.id, status: "in_progress" })
+    context 'in_progress' do
+
+      let!(:game) { create(:game) }
+      let!(:frames) { FactoryBot.create_list(:frame, 3, game_id: game.id) }
+     
+      let(:service) do
+        described_class.call(game)
+      end
+      
+      it 'is a game in_progress' do
+        expect(service.value[:status]).to eq("in_progress")
+      end
+    end
+
+    context 'finished' do
+
+      let!(:game) { create(:game) }
+      let!(:frames) { FactoryBot.create_list(:frame, 10, game: game) }
+      let!(:attempts) { FactoryBot.create_list(:attempt, 3, frame: frames.last) }
+     
+      let(:service) do
+        described_class.call(game)
+      end
+      
+      it 'is a game in_finished' do
+        expect(service.value[:status]).to eq("finished")
+      end
+    end
+
+    context 'attempts' do
+
+      let!(:game) { create(:game) }
+      let!(:frames) { FactoryBot.create_list(:frame, 7, game_id: game.id) }
+      let!(:attempts) do
+        frames.each do |frame|
+          Attempt.create!(frame: frame, overtuned_pins: rand(1..9))
+          Attempt.create!(frame: frame, overtuned_pins: rand(1..9))
+        end
+      end
+
+      let(:service) do
+        described_class.call(game)
+      end
+
+      it 'has 7 frames' do
+        expect(service.value.dig(:frames).count).to eq(7)
+      end
     end
   end 
 
