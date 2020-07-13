65d78b017fa097d5554cb20a0b362fe0cfda659f
Anchieta Júnior <santosjr87@gmail.com>
2020-07-12 21:37:32 -0300

    Refactor current frame service

--- app/services/frames/current_frame_service.rb
+++ app/services/frames/current_frame_service.rb
@@ -25,14 +25,14 @@ module Frames
 
     def finished?
       return true if tenth_frame_finished?
-      return true if strike?(last_frame) && !tenth_frame?
-      return true if spare?(last_frame) && !tenth_frame?
+      return true if strike? && !tenth_frame?
+      return true if spare? && !tenth_frame?
       false
     end
 
     def tenth_frame_finished?
       return true if tenth_frame? && last_frame_attempts == 3
-      return true if tenth_frame? && spare?(last_frame)
+      return true if tenth_frame? && spare?
       false
     end
 
@@ -40,12 +40,12 @@ module Frames
       game_frames_count == 10
     end
 
-    def strike?(frame)
-      frame.attempts.order(:created_at).first.overtuned_pins == 10
+    def strike?
+      last_frame.attempts.order(:created_at).first.overtuned_pins == 10
     end
 
-    def spare?(frame)
-      frame.attempts.order(:created_at).first.overtuned_pins < 10 && last_frame_attempts == 2
+    def spare?
+      last_frame.attempts.order(:created_at).first.overtuned_pins < 10 && last_frame_attempts == 2
     end
 
     def last_frame_attempts
