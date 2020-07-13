95ee7058245cd12b78c54809da7566f1effe38db
Anchieta Júnior <santosjr87@gmail.com>
2020-07-12 22:25:48 -0300

    Add specs to attempt creator service

--- spec/services/attempts/attempt_creator_service_spec.rb
+++ spec/services/attempts/attempt_creator_service_spec.rb
@@ -100,8 +100,36 @@ RSpec.describe Attempts::AttemptCreatorService do
         described_class.call(params)
       end
 
-      it 'not allowed create a third attempt when the last frame does not have a strike' do
+      it 'is not allowed to create a third attempt when the last frame does not have a strike' do
         expect(service.success?).to be_falsey
+      end
+    end
+
+    context 'can create attempt in the tenth frame' do
+      let!(:game)     { create(:game) }
+      let!(:frames) { FactoryBot.create_list(:frame, 10, game: game) }
+      
+      let!(:attempts) do
+        frames.each_with_index do |frame, index|
+          Attempt.create!(frame: frame, overtuned_pins: 10) if index == 9
+          Attempt.create!(frame: frame, overtuned_pins: 4)
+        end
+      end
+
+      let(:params) do
+        {
+          game_id: game.id,
+          overtuned_pins: 7
+        }
+      end
+
+      let(:service) do
+        described_class.call(params)
+      end
+
+      it 'is allowed to create a third attempt when the last frame has a strike' do
+        service
+        expect(game.frames.last.attempts.count).to eq(3)
       end
     end
   end
